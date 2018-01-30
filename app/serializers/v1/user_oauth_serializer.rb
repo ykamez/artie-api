e32c2a2b3f3143daacc758ffe962d4209a79f454
kame <ky.syogun@gmail.com>
2018-01-30 11:32:59 +0900

    Fix omniauth response

---
+++ app/serializers/v1/user_oauth_serializer.rb
@@ -0,0 +1,21 @@
+module V1
+  class UserOauthSerializer < ActiveModel::Serializer
+    attributes :id, :full_name, :image_url, :total_likes_count, :followings_count, :followers_count, :evaluation_point, :access_token, :uid, :client
+
+    def total_likes_count
+      object.likes_count
+      end
+
+    def followings_count
+      object.following_count
+    end
+
+    def image_url
+      object.image_data
+    end
+
+    def full_name
+      object.fullname
+    end
+  end
+end
