01e1db9b73a48d15c8d3641d6192c3dcb82071ca
kame <ky.syogun@gmail.com>
2018-02-01 09:40:37 +0900

    refactor: delete unused code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,38 +1,18 @@
 class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
-  # 　全体的にhttps://qiita.com/AQeNku/items/94485432184257799106を参考に実装中。
-
-  # def twitter
-  #   auth = request.env['omniauth.auth']
-  #   user = User.find_or_create_from_auth(auth)
-  #   render json: user, serializer: ::V1::UserSerializer
-  # end
-
-  # https://stackoverflow.com/questions/37225478/the-action-redirect-callbacks-could-not-be-found-for-usersomniauthcallbacksc
-
+  # 参考: https://qiita.com/AQeNku/items/94485432184257799106
   def redirect_callbacks
-    # https://github.com/lynndylanhurley/devise_token_auth/issues/586
-    # use auth/twitter, not omniauth/twitter
+    # 参考: https://github.com/lynndylanhurley/devise_token_auth/issues/586
     super
   end
 
-  # Ths method overrides inherited controllers' method because some features is unused.
   def omniauth_success
     get_resource_from_auth_hash
     create_token_info
     set_token_on_resource
     create_auth_params
-
-    # 使わないのでコメントアウト
-    # if resource_class.devise_modules.include?(:confirmable)
-    #  # don't send confirmation email!!!
-    #  @resource.skip_confirmation!
-    # end
-
     @resource.skip_confirmation!
     sign_in(:user, @resource, store: false, bypass: false)
-
     if @resource.save!
-      # update_token_authをつけることでレスポンスヘッダーに認証情報を付与できる。
       update_auth_header
       yield @resource if block_given?
       @data = {
@@ -65,14 +45,10 @@ class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksCon
 
       if @resource.new_record?
         @oauth_registration = true
-        # これが呼ばれるとエラーになるのでコメントアウト by qiita
-        # set_random_password
       end
 
-      # sync user info with provider, update/generate auth token
       assign_provider_attrs(@resource, auth_hash.with_indifferent_access)
 
-      # assign any additional (whitelisted) attributes
       extra_params = whitelisted_params
       @resource.assign_attributes(extra_params) if extra_params
 
